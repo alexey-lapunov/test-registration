77f5e6c57027865ee4e0465fafeb8a49174a644c
Alexey <leshka.lapunov@gmail.com>
2019-11-14 23:54:13 +0300

    progress login page

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 
+import LoginPage from './../LoginPage/';
+
 import styles from './App.module.scss';
 
 function App() {
   return (
-    <div>
+    <div className={styles.app}>
       <h1>Test registration</h1>
+      <LoginPage />
     </div>
   );
 }
---
+++ src/pages/LoginPage/LoginPage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class LoginPage extends React.PureComponent {
+  render() {
+    return <span>Login page</span>;
+  }
+}
+
+export default LoginPage;
